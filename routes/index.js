c7cac1f7e6afbf2bbad5575250d0acc81443caa7
Hung Doan <hdoan741@gmail.com>
2012-09-23 06:41:47 +0800

    update home view.

--- routes/index.js
+++ routes/index.js
@@ -5,13 +5,33 @@ var modelProvider = require('../models/model').instance;
 
 exports.index = function(req, res) {
   if (!req.user) {
-    res.render('index', { title: 'Express' });
+    var tut = {
+      id: 10,
+      title: 'NodeJS web basic',
+      createdBy: 'Hung Doan',
+      star: 5,
+      desc: "In this tutorial I will run you through the process setting up an Express.js app and making it do what a basic website might do. You will learn the basics of routes, views, Jade templates, Stylus CSS engine, handling POST and GET requests.",
+      url: 'http://www.hacksparrow.com/express-js-tutorial.html',
+      stat: {
+        likes: 103,
+        shares: 50,
+        views: 1042
+      },
+      acquires: [
+        'nodejs-basic',
+        'javascript',
+      ],
+    };
+    var data = {
+      title: 'LearnPath!',
+      popular_tuts: [tut, tut, tut],
+      new_tuts: [tut, tut]
+    };
+    res.render('index', data);
   } else {
-    res.render('index', { title: 'Hello ' + req.user });
+    res.render('index', { title: req.user });
   }
 };
-
-
 
 exports.learnpath = function(req, res) {
   console.log(req.params);
@@ -113,7 +133,6 @@ exports.profile = function(req, res){
   }
   res.render('profile', {user: user});
 };
-
 
 exports.topic_hint = function(req, res) {
   var query = req.query.q;
