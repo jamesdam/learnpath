2f36e48d3a5743ac5578b1eced3b74513c4f7966
James <damtuanlong@gmail.com>
2012-09-23 00:55:56 +0800

    learnpath with sidebar

--- routes/index.js
+++ routes/index.js
@@ -52,7 +52,28 @@ exports.tutorial = function(req, res) {
 
 exports.learnpath = function(req, res) {
   console.log(req.params);
-  res.render('learnpath',{ title: 'Path title' });
+  var pathId = req.params.lid;
+  if (!pathId) {
+    res.send("path Id not defined");
+  } else {
+    // fetch tutorial based on id
+    var path = {
+      title: 'NodeJS web basic',
+      createdBy: 'Hung Doan',
+      star: 5,
+      desc: "In this tutorial I will run you through the process setting up an Express.js app and making it do what a basic website might do. You will learn the basics of routes, views, Jade templates, Stylus CSS engine, handling POST and GET requests.",
+      url: 'http://www.hacksparrow.com/express-js-tutorial.html',
+      stat: {
+        likes: 103,
+        shares: 50,
+        views: 1042
+      }
+    };
+    res.render('learnpath', {
+      title: path.title,
+      path: path
+    });
+  }
 }
 
 /*
