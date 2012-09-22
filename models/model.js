86456663fde220e15514ebad57a491492953b9d3
Tran Cong Hoang <hoang.trancong90@gmail.com>
2012-09-22 22:25:53 +0000

    add share like models

--- models/model.js
+++ models/model.js
@@ -21,8 +21,8 @@ function createSchema(mongoose, fn) {
 	var Schema = mongoose.Schema;
 	var ObjectId = Schema.ObjectId;
 	var userSchema = new Schema({
-			user_name: String,
-			user_email: String,
+			name: String,
+			email: String,
 			listLearnedPath: 
 			[
 			{
@@ -41,11 +41,32 @@ function createSchema(mongoose, fn) {
 				tutorialId: ObjectId
 			}
 			],
+			listLikedTutorial: [
+			{
+				tutorialId: ObjectId
+
+			}],
+			listSharedTutorial: [
+			{
+				tutorialId: ObjectId
+			}
+			],
+			listLikedPath: [
+			{
+				pathId: ObjectId
+
+			}],
+			listSharedPath: [
+			{
+				pathId: ObjectId
+			}
+			],
 			listLearnedPath: [
 			{
 				pathId: ObjectId
 			}
 			]});
+
 
 	userSchema.virtual('id')
 	.get(function() {
@@ -87,23 +108,55 @@ function createSchema(mongoose, fn) {
 		requiredTopics: [
 		{
 			topicId: ObjectId
+		}], 
+		likeUsers: [
+		{
+			userId: ObjectId
+		} 
+		],
+		shareUsers: [
+		{
+			userId: ObjectId
 		}
 		]
+
 	});
 
+	tutorialSchema.virtual('id')
+	.get(function() {
+		return this._id.toHexString();
+	});
 
 	var Tutorial = mongoose.model('Tutorial', tutorialSchema);
 
 	var pathSchema = new Schema( {
 		name: String,
 		description: String,
+		baseRequirement: [
+		{
+			userId: ObjectId
+		}],
 		content: [
 		{
 			step: Number,
 			type: String,
 			itemId: ObjectId,
+		}],		
+		likeUsers: [
+		{
+			userId: ObjectId
+		} 
+		],
+		shareUsers: [
+		{
+			userId: ObjectId
 		}
 		]
+	});
+
+	pathSchema.virtual('id')
+	.get(function() {
+		return this._id.toHexString();
 	});
 
 	var Path = mongoose.model('Path', pathSchema);
@@ -135,6 +188,26 @@ ModelProvider.prototype.findTopicById = function(id, callback) {
 
 ModelProvider.prototype.findTutorialById = function(id, callback) {
 	this.Tutorial.findById(id, function(err, tuts) {
+		if (err)
+			callback(err);
+		else {
+			callback(null, tuts);
+		}
+	});
+}
+
+ModelProvider.prototype.findUserById = function(id, callback) {
+	this.User.findById(id, function(err, tuts) {
+		if (err)
+			callback(err);
+		else {
+			callback(null, tuts);
+		}
+	});
+}
+
+ModelProvider.prototype.findPathById = function(id, callback) {
+	this.Path.findById(id, function(err, tuts) {
 		if (err)
 			callback(err);
 		else {
@@ -198,5 +271,104 @@ ModelProvider.prototype.createTutorial = function(tut, callback) {
 			callback(null, tutorial._id.toHexString());
 		}
 	});
+}
+
+
+ModelProvider.prototype.createUser = function(us, callback) {
+	var user = this.User();
+	user.name = us.name;
+	user.email = us.email;
+	user.listLearnedPath = [];
+	user.LearnedTopic = [];
+	user.listLearnedTutorial = [];
+	user.listLikedTutorial = [];
+	user.listSharedTutorial = [];
+	user.listLikedPath = [];
+	user.listSharedPath = [];
+	user.listLearnedPath = [];
+	user.save(function(err, user) {
+		if (err) 
+			callback(err);
+		else {
+			callback(null, user.toObject()._id.toHexString());
+		}
+	});
+}
+
+ModelProvider.prototype.createPath = function(pa, callback) {
+	var path = this.Path();
+	path.name = pa.name;
+	path.description = pa.description;
+	path.content = [];
+	path.likeUsers = [];
+	path.shareUsers = [];
+	path.save(function(err, path) {
+		if (err) 
+			callback(err);
+		else {
+			callback(null, path.toObject()._id.toHexString());
+		}
+	});
+}
+
+
+ModelProvider.prototype.addUserLikePath = function(userId, pathId, callback) {
+	var user = findUserById(userId);
+	if (user.listLikedPath.indexOf(pathId) !== -1) {
+		var path = findPathById(pathId);
+		user.listLikedPath.push(createFromHexString(pathId));
+		path.likeUsers.push(createFromHexString(userId));
+		user.save();
+		path.save();
+		callback(null);
+	} else {
+		var err = "Duplicate user-path like";
+		callback(err);
+	}
+}
+
+ModelProvider.prototype.addUserSharePath = function(userId, pathId, callback) {
+	var user = findUserById(userId);
+	if (user.listSharedPath.indexOf(pathId) !== -1) {
+		var path = findPathById(pathId);
+		user.listSharedPath.push(createFromHexString(pathId));
+		path.shareUsers.push(createFromHexString(userId));
+		user.save();
+		path.save();
+		callback(null);
+	} else {
+		var err = "Duplicate user-path share";
+		callback(err);
+	}
+}
+
+ModelProvider.prototype.addUserLikeTutorial = function(userId, tutorialId, callback) {
+	var user = findUserById(userId);
+	if (user.listLikedTutorial.indexOf(tutorialId) !== -1) {
+		var tut = findTutorialById(pathId);
+		user.listLikedTutorial.push(createFromHexString(tutorialId));
+		tut.likeUsers.push(createFromHexString(userId));
+		user.save();
+		tut.save();
+		callback(null);
+	} else {
+		var err = "Duplicate user-tut like";
+		callback(err);
+	}
+}
+
+ModelProvider.prototype.addUserShareTutorial = function(userId, tutorialId, callback) {
+	var user = findUserById(userId);
+	if (user.listSharedTutorial.indexOf(tutorialId) !== -1) {
+		var tut = findTutorialById(pathId);
+		user.listSharedTutorial.push(createFromHexString(tutorialId));
+		path.shareUsers.push(createFromHexString(userId));
+		user.save();
+		tut.save();
+		callback(null);
+	} else {
+		var err = "Duplicate user-tut share";
+		callback(err);
+	}
 }
 
