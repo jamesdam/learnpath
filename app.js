60e8bfe139dd824fd36e6b76334c20321f8379cc
James <damtuanlong@gmail.com>
2012-09-22 17:09:01 +0800

    route login

--- app.js
+++ app.js
@@ -29,6 +29,7 @@ app.configure('development', function(){
 
 app.get('/', routes.index);
 app.get('/users', user.list);
+app.get('/login', routes.login);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,14 @@
 /*
  * GET home page.
  */
-
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Learn Path' });
+};
+
+/*
+ * GET login page.
+ */
+exports.login = function(req, res){
+  console.log('login');	
+  res.render('login',{ title: 'Login' });
 };
