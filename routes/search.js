11630cde6010b94c0f540e088f5e352a433d8866
Hung Doan <hdoan741@gmail.com>
2012-09-23 03:15:45 +0800

    update search result.

--- routes/search.js
+++ routes/search.js
@@ -4,6 +4,40 @@
  */
 
 exports.list = function(req, res){
-  // res.send("respond with a resource");
-  res.render('search', { title: 'Search Result' });
+
+  var query = req.query.q;
+  var tut = {
+          title: 'NodeJS web basic',
+          id: 10,
+          createdBy: 'Hung Doan',
+          desc: "In this tutorial I will run you through the process setting up an Express.js app and making it do what a basic website might do. You will learn the basics of routes, views, Jade templates, Stylus CSS engine, handling POST and GET requests.",
+          stat: {
+            likes: 103,
+            shares: 50,
+            views: 1042
+          },
+          acquires: [
+            'expressjs-basic',
+            'nodejs-basic',
+            'javascript',
+          ]
+        };
+
+  var search_result = {
+    title: '$earch Result',
+    query: query,
+    results: [
+      { level: 'Beginner',
+        tuts: [tut, tut, tut]
+      },
+      { level: 'Intermediate',
+        tuts: [tut, tut]
+      },
+      { level: 'Advanced',
+        tuts: [tut, tut, tut]
+      }
+    ]
+  }
+
+  res.render('search', search_result);
 };
