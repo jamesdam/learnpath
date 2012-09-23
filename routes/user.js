ad115cd8601166a5322226166de5a5976d3a2589
James <damtuanlong@gmail.com>
2012-09-23 11:07:03 +0800

    model user with  profile

--- routes/user.js
+++ routes/user.js
@@ -1,33 +1,24 @@
-
-/*
- * GET users listing.
- */
+var modelProvider = require('../models/model').instance;
 
 exports.profile = function(req, res){
 var uid = req.params.uid;
-
-  var user = {
-    id : uid,
-    name : uid,
+console.log("uid " + uid);
+	modelProvider.findUserByName(uid,function(err,result){
+		console.log("user " + result);
+	var user = {
+    id : result.uid,
+    name : result.name,
     about: "I am a programmer",
     stat: {
       friends: 19,
       paths : 3,
       tutorials : 45
     },
-    skills : ["javascript","javascript-basic","nodejs","nodejs-intermediate","expressjs-basic","openVC-basic","objectiveC-basic"],
-    completedPaths : [
-      { name :"basic javascript" , url : "http://google.com"},
-      { name :"basic javascript" , url : "http://google.com"},
-      { name :"basic javascript" , url : "http://google.com"}
-    ],
-    completedTutorials: [
-      { name :"basic javascript" , url : "http://google.com"},
-      { name :"basic javascript" , url : "http://google.com"},
-      { name :"basic javascript" , url : "http://google.com"},
-      { name :"basic javascript" , url : "http://google.com"},
-      { name :"basic javascript" , url : "http://google.com"}
-    ]
-  }
+    skills : result.listLearnedTopic,
+    completedPaths : result.listLearnedPath,
+    completedTutorials :result.listLearnedTutorial
+   }
   res.render('profile', {user: user});
+	});
+  
 };
